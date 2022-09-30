65d93e1c2332057a0a9e1bfac47f176187b03791
“preetimca2016” <preetimca2016@gmail.com>
2022-09-30 23:32:58 +0530

    preeti add program.cs

---
+++ ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class ProductReview
+    {
+        //property
+        public int ProductID 
+        {
+            get; 
+            set; 
+        }
+        public int UserID 
+        { 
+            get; 
+            set;
+        }
+        public double Rating 
+        { 
+            get;
+            set; 
+        }
+        public string Review
+        { 
+            get; 
+            set; 
+        }
+        public bool isLike 
+        {
+            get; 
+            set;
+        }
+    }
+}
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductReviewManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review management Problem statement");
+        }
+    }
+}
