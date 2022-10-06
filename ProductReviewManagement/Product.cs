f9c7514f57af3750c752cfef13d69b1e7f74da93
“preetimca2016” <preetimca2016@gmail.com>
2022-10-06 08:46:52 +0530

    Added: ProductDetails

---
+++ ProductReviewManagement/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+
+    public class Products
+    {
+        public static void AddingProductReview(List<ProductReviews> products)
+        {
+            products.Add(new ProductReviews() { productId = 1, userId = 1, review = "Good", rating = 14, isLike = true });
+            products.Add(new ProductReviews() { productId = 2, userId = 2, review = "Average", rating = 12, isLike = true });
+            products.Add(new ProductReviews() { productId = 3, userId = 4, review = "Good", rating = 19, isLike = true });
+            products.Add(new ProductReviews() { productId = 2, userId = 5, review = "Bad", rating = 7, isLike = false });
+            products.Add(new ProductReviews() { productId = 1, userId = 1, review = "Very Good", rating = 19, isLike = true });
+            products.Add(new ProductReviews() { productId = 2, userId = 6, review = "Average", rating = 10, isLike = true });
+            products.Add(new ProductReviews() { productId = 4, userId = 7, review = "Good", rating = 15, isLike = true });
+            products.Add(new ProductReviews() { productId = 9, userId = 8, review = "Average", rating = 11, isLike = true });
+            products.Add(new ProductReviews() { productId = 3, userId = 9, review = "Bad", rating = 6, isLike = false });
+            products.Add(new ProductReviews() { productId = 5, userId = 4, review = "Average", rating = 13, isLike = true });
+            DisplayRecord(products);
+        }
+        public static void DisplayRecord(List<ProductReviews> products)
+        {
+            foreach (ProductReviews product in products)
+            {
+                Console.WriteLine("ProductId:{0}\t UserId:{1}\t Review:{2}\tRating:{3}\tIsLike:{4}\t", product.productId, product.userId, product.review, product.rating, product.isLike);
+            }
+        }
+        public static void RetrieveTopThreeRating(List<ProductReviews> products)
+        {
+            AddingProductReview(products);
+            Console.WriteLine("\nRetrieving Top Three Records Based On Rating\n");
+            var res = (from product in products orderby product.rating descending select product).Take(3).ToList();
+            DisplayRecord(res);
+
+        }
+    }
+}
--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to product review management Problem statement");
+            Console.WriteLine("Welcome to product review management system");
+            Console.WriteLine("\n1.Add Values to list \n2.Retrieve Top 3 Records\nEnter Option: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            //Creating a list for Product Review 
+            List<ProductReviews> productReviews = new List<ProductReviews>();
+            switch (option)
+            {
+                case 1:
+                    Products.AddingProductReview(productReviews);
+                    break;
+                case 2:
+                    Products.RetrieveTopThreeRating(productReviews);
+                    break;
+            }
         }
     }
 }
